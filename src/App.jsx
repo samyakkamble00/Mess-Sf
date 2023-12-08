f6da71e8f998e3a0cc95e08f7e2f9d66232c4d66
samyakkamble240500 <samyakk07@gmail.com>
2023-12-08 21:39:40 +0530

    App Change

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,6 @@ const App = () => {
     <AnimatePresence>
       <div className="w-screen h-auto flex flex-col bg-primary">
         <Header />
-
         <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
           <Routes>
             <Route path="/*" element={<MainContainer />} />
